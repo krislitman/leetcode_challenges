5aac8cb7c9d5150828cff2793b87f88d0ee8f2ca
Kris Litman <kris.d.litman@gmail.com>
2021-07-19 11:30:21 -0400

    Working on reverse linked list challenge

--- palindrome.rb
+++
@@ -1,9 +0,0 @@
-def is_palindrome(s)
-  s = s.gsub(/[^A-Za-z0-9 ]/, '')
-  forwards = s.downcase.delete(" ").split("")
-  backwards = forwards.reverse
-  return true if backwards == forwards
-  false
-end
-
-print is_palindrome("A man, a plan, a canal: Panama")
