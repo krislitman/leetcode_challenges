2638f64abb64e29f695bc807f0fd5c2a9d11d0af
Kris Litman <kris.d.litman@gmail.com>
2021-07-28 15:38:36 -0400

    Working on second iteration of merge_intervals

--- ruby/merge_intervals.rb
+++ ruby/merge_intervals.rb
@@ -20,22 +20,28 @@ def merge(intervals)
   l = intervals.length
   c = 0
   while c < (l - 1)
-    if intervals[c].max >= intervals[c + 1].min
-      new_array.append(intervals[c].min)
-      new_array.append(intervals[c + 1].max)
-      intervals.delete_at(c + 1)
-      intervals.delete_at(c)
-      intervals.append(new_array)
-      c+=1
+    if intervals[c]
+      if intervals[c].max >= intervals[c + 1].min
+        new_array.append(intervals[c].min)
+        new_array.append(intervals[c + 1].max)
+        intervals.delete_at(c + 1)
+        intervals.delete_at(c)
+        intervals.append(new_array)
+        c+=1
+      else
+        c += 1
+      end
     else
+      c += 1
+      next
     end
   end
   return intervals
 end
 
-attempt1 = merge([[1,4],[4,5]])
-print attempt1
-# TODO expected [[1,5]]
+# attempt1 = merge([[1,4],[4,5]])
+# print attempt1
+# # TODO expected [[1,5]]
 
 attempt2 = merge([[1,3],[2,6],[8,10],[15,18]])
 print attempt2
