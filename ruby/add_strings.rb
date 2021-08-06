9b01953aa6b7fa4e4eb2eec87ad3157bac349720
Kris Litman <kris.d.litman@gmail.com>
2021-08-05 21:08:15 -0400

    Created first attempt setup

---
+++ ruby/add_strings.rb
@@ -0,0 +1,28 @@
+# Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+
+# You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the inputs to integers directly.
+
+
+
+# Example 1:
+
+# Input: num1 = "11", num2 = "123"
+# Output: "134"
+# Example 2:
+
+# Input: num1 = "456", num2 = "77"
+# Output: "533"
+# Example 3:
+
+# Input: num1 = "0", num2 = "0"
+# Output: "0"
+
+def add_strings(num1, num2)
+  require 'pry'; binding.pry
+  # new_num = num1.to_i += num2.to_i
+  # new_num.to_s
+end
+
+attempt1 = add_strings('11', '123')
+print(attempt1)
+# expect: '134'
