e148da6e39dadbda471d6e29c44d11b3d030d31b
Kris Litman <kris.d.litman@gmail.com>
2021-08-14 22:05:09 -0400

    Passing attempts

--- ruby/jump_game_i.rb
+++ ruby/jump_game_i.rb
@@ -16,9 +16,33 @@
 # Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
 
 def can_jump(nums)
-
+  e = (nums.length - 1)
+  l = nums.length
+  c = 0
+  case
+  when nums.empty?
+    return false
+  when !nums.empty?
+    until l == 0 do
+      n = nums[c]
+      if n == e
+        return true
+      elsif (n == 0) && (c != e) && (l != nums.length)
+        return false
+      elsif c == e
+        return true
+      else
+        c += n
+        l -= 1
+      end
+    end
+  end
 end
 
 attempt1 = can_jump([2,3,1,1,4])
 print(attempt1)
 # expect: true
+
+attempt2 = can_jump([3,2,1,0,4])
+print(attempt2)
+# expect: false
