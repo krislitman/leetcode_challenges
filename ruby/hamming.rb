831a21f48b1fe497173152ef840f4067f7eb1186
Kris Litman <kris.d.litman@gmail.com>
2021-09-05 19:21:09 -0400

    Looked up binary representation methods for ruby

--- ruby/hamming.rb
+++ ruby/hamming.rb
@@ -1,11 +1,6 @@
 # The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
-
-# Given an integer array nums, return the sum of Hamming distances between all the pairs of the integers in nums.
-
-
-
+# Given an integer array nums, return the sum of Hamming distances between all the pairs of the integers in nums
 # Example 1:
-
 # Input: nums = [4,14,2]
 # Output: 6
 # Explanation: In binary representation, the 4 is 0100, 14 is 1110, and 2 is 0010 (just
@@ -13,12 +8,12 @@
 # The answer will be:
 # HammingDistance(4, 14) + HammingDistance(4, 2) + HammingDistance(14, 2) = 2 + 2 + 2 = 6.
 # Example 2:
-
 # Input: nums = [4,14,4]
 # Output: 4
 
 def total_hamming_distance(nums)
-
+  converted_nums = nums.map{|n| "%04b" % n}
+  require 'pry'; binding.pry
 end
 
 attempt1 = total_hamming_distance([4,14,2])
