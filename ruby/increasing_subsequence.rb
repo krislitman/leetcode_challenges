4af1de44b6d7f789c65e5d5417df1fba0728c025
Kris Litman <kris.d.litman@gmail.com>
2021-11-26 21:20:27 -0500

    first test case is working

--- ruby/increasing_subsequence.rb
+++ ruby/increasing_subsequence.rb
@@ -14,12 +14,34 @@
 # Output: [[4,4]]
 
 
-def find_subsequences(nums)
+@c = 0
+@result = []
+@l = nil
 
+def find_subsequences(nums)
+  s = []
+  require 'pry'; binding.pry
+  return @result if nums.length == 0
+  return [nums] if @result.empty? && nums.length == 1
+
+  @c = @c + 1
+  nums.each do |n|
+    s << n
+    if n < nums[@c]
+      next
+    elsif n > nums[@c]
+      s << nums[@c]
+      find_subsequences(nums)
+    end
+  end
 end
 
-nums1 = [4,6,7,7]
-attempt1 = find_subsequences(nums1) # expected => [[4,6],[4,6,7],[4,6,7,7],[4,7],[4,7,7],[6,7],[6,7,7],[7,7]]
+nums3 = [4]
+puts find_subsequences(nums3)
+
+# nums1 = [4,6,7,7]
+# attempt1 = find_subsequences(nums1) # expected => [[4,6],[4,6,7],[4,6,7,7],[4,7],[4,7,7],[6,7],[6,7,7],[7,7]]
+# puts attempt1
 
 # nums2 = [4,4,3,2,1]
 # attempt2 = find_subsequences(nums2) # expected => [[4,4]]
