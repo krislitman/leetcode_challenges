3b40bd2365bfc4612756cfcd18411223be96ea05
Kris Litman <kris.d.litman@gmail.com>
2021-11-26 20:54:53 -0500

    Added test cases

--- ruby/increasing_subsequence.rb
+++ ruby/increasing_subsequence.rb
@@ -1,14 +1,25 @@
 # Given an integer array nums, return all the different possible increasing subsequences of the given array with at least two elements. You may return the answer in any order.
-# 
+#
 # The given array may contain duplicates, and two equal integers should also be considered a special case of increasing sequence.
-# 
-# 
-# 
+#
+#
+#
 # Example 1:
-# 
+#
 # Input: nums = [4,6,7,7]
 # Output: [[4,6],[4,6,7],[4,6,7,7],[4,7],[4,7,7],[6,7],[6,7,7],[7,7]]
 # Example 2:
-# 
+#
 # Input: nums = [4,4,3,2,1]
 # Output: [[4,4]]
+
+
+def find_subsequences(nums)
+
+end
+
+nums1 = [4,6,7,7]
+attempt1 = find_subsequences(nums1) # expected => [[4,6],[4,6,7],[4,6,7,7],[4,7],[4,7,7],[6,7],[6,7,7],[7,7]]
+
+# nums2 = [4,4,3,2,1]
+# attempt2 = find_subsequences(nums2) # expected => [[4,4]]
