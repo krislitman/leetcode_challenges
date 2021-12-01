32e1c04c7d1ea68958198832f092e5d39ac54241
Kris Litman <kris.d.litman@gmail.com>
2021-11-30 20:57:16 -0500

    Test case three passing

--- ruby/difference.rb
+++ ruby/difference.rb
@@ -5,7 +5,7 @@
 # Return the letter that was added to t.
 
 def find_the_difference(s, t)
-  return if s.length == 0 || t.length == 0
+  return if s.length == 0 && t.length == 0
   split = t.split("")
   compare = s.split("")
   found = nil
@@ -22,8 +22,11 @@ end
 # attempt1 = find_the_difference("", "")
 # puts attempt1
 
-attempt2 = find_the_difference("abcd", "abcde")
-puts attempt2
+# attempt2 = find_the_difference("abcd", "abcde")
+# puts attempt2
+
+attempt3 = find_the_difference("", "y")
+puts attempt3
 
 # Example 1:
 
