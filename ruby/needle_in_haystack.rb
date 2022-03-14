0ed958b0927c0cc5dc6c20790d8480cceb82ea31
Kris Litman <kris.d.litman@gmail.com>
2022-03-14 19:11:04 -0400

    Added ruby needle in haystack

---
+++ ruby/needle_in_haystack.rb
@@ -0,0 +1,23 @@
+# What should we return when needle is an empty string? This is a great question to ask during an interview.
+
+# For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+
+# @param {String} haystack
+# @param {String} needle
+# @return {Integer}
+def str_str(haystack, needle)
+
+end
+
+# Example 1:
+
+# Input: haystack = "hello", needle = "ll"
+# Output: 2
+# Example 2:
+
+# Input: haystack = "aaaaa", needle = "bba"
+# Output: -1
+# Example 3:
+
+# Input: haystack = "", needle = ""
+# Output: 0
