0586edd60fbdf59c82ab708b8587a41555fe08d8
Kris Litman <kris.d.litman@gmail.com>
2022-04-25 23:09:20 -0400

    Built out test cases

--- ruby/move_zeros.rb
+++ ruby/move_zeros.rb
@@ -5,3 +5,20 @@
 def move_zeroes(nums)
 
 end
+
+
+
+# Example 1:
+
+one =  [0,1,0,3,12]
+puts move_zeroes(one)
+
+# Output: [1,3,12,0,0]
+
+# Example 2:
+
+# two =  [0]
+# puts move_zeroes(two)
+
+# Input: nums = [0]
+# Output: [0]
