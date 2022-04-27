92bfdf77d47c6abdcc36e322eca7e29d1b7a5775
Kris Litman <kris.d.litman@gmail.com>
2022-04-26 21:58:19 -0400

    First test passing

--- ruby/missing_number.rb
+++ ruby/missing_number.rb
@@ -1,7 +1,18 @@
 # Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
 
 def find_disappeared_numbers(nums)
-    nums
+    found = []
+    l = nums.length
+    if !nums.include?(l)
+        found << l
+    end
+    r = (nums.min..nums.max)
+    r.each do |i|
+        if !nums.include?(i)
+            found << i
+        end
+    end
+    found
 end
 
 
