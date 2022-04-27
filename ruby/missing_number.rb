9e1c4f676ea00226808a6f8e04a167c37d883d9f
Kris Litman <kris.d.litman@gmail.com>
2022-04-26 21:47:33 -0400

    Added in method

--- ruby/missing_number.rb
+++ ruby/missing_number.rb
@@ -1,5 +1,8 @@
 # Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
 
+def find_disappeared_numbers(nums)
+
+end
 
 
 #     Example 1:
