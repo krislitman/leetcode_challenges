c611f63d1389664a3ca797cfea840ef567b318e9
Kris Litman <kris.d.litman@gmail.com>
2022-04-26 21:49:39 -0400

    Using print vs puts

--- ruby/missing_number.rb
+++ ruby/missing_number.rb
@@ -1,15 +1,15 @@
 # Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
 
 def find_disappeared_numbers(nums)
-
+    nums
 end
 
 
 #     Example 1:
 
-puts find_disappeared_numbers([4,3,2,7,8,2,3,1])
+print find_disappeared_numbers([4,3,2,7,8,2,3,1])
 #     Output: [5,6]
 #     Example 2:
 
-#puts find_disappeared_numbers([1,1])
+#print find_disappeared_numbers([1,1])
 #     Output: [2]
