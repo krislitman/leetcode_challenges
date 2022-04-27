4d3fc9711e775c2f91a7c46d8a2b901558e5bd77
Kris Litman <kris.d.litman@gmail.com>
2022-04-26 21:48:50 -0400

    Built test cases

--- ruby/missing_number.rb
+++ ruby/missing_number.rb
@@ -7,9 +7,9 @@ end
 
 #     Example 1:
 
-#     Input: nums = [4,3,2,7,8,2,3,1]
+puts find_disappeared_numbers([4,3,2,7,8,2,3,1])
 #     Output: [5,6]
 #     Example 2:
 
-#     Input: nums = [1,1]
+#puts find_disappeared_numbers([1,1])
 #     Output: [2]
