3a13b2a8822b521ab7fdc119480dd53f25a793cd
Kris Litman <kris.d.litman@gmail.com>
2022-04-26 21:58:39 -0400

    All tests passing

--- ruby/missing_number.rb
+++ ruby/missing_number.rb
@@ -22,5 +22,5 @@ print find_disappeared_numbers([4,3,2,7,8,2,3,1])
 #     Output: [5,6]
 #     Example 2:
 
-#print find_disappeared_numbers([1,1])
+print find_disappeared_numbers([1,1])
 #     Output: [2]
