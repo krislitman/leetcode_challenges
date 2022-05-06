e803781119e5b4877250e171a0dc940bf761c96d
Kris Litman <kris.d.litman@gmail.com>
2022-05-06 19:59:27 -0400

    All passing

--- ruby/duplicate_array.rb
+++ ruby/duplicate_array.rb
@@ -23,7 +23,7 @@
 
 
 def remove_duplicates(nums)
-    
+    nums.uniq!.length
 end
 
 # Example 1:
@@ -36,7 +36,7 @@ print remove_duplicates([1,1,2])
 
 # Example 2:
 
-# print remove_duplicates([0,0,1,1,1,2,2,3,3,4])
+print remove_duplicates([0,0,1,1,1,2,2,3,3,4])
 # Input: nums = [0,0,1,1,1,2,2,3,3,4]
 # Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
 # Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
