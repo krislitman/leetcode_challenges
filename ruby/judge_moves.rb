e3cd5778312d5c0d8b68fe439b46234dfbea8bfe
Kris Litman <kris.d.litman@gmail.com>
2022-05-05 21:47:23 -0400

    First test case

--- ruby/judge_moves.rb
+++ ruby/judge_moves.rb
@@ -13,6 +13,8 @@ end
 
 # Example 1:
 
+print judge_circle("UD")
+
 # Input: moves = "UD"
 # Output: true
 # Explanation: The robot moves up once, and then down once. All moves have the same magnitude, so it ended up at the origin where it started. Therefore, we return true.
