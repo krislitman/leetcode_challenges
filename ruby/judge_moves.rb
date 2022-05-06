f32503848c229c7f2bb343b29fed7c47498ee327
Kris Litman <kris.d.litman@gmail.com>
2022-05-05 21:57:09 -0400

    Both tests passing

--- ruby/judge_moves.rb
+++ ruby/judge_moves.rb
@@ -33,6 +33,8 @@ print judge_circle("UD")
 # Explanation: The robot moves up once, and then down once. All moves have the same magnitude, so it ended up at the origin where it started. Therefore, we return true.
 # Example 2:
 
+print judge_circle("LL")
+
 # Input: moves = "LL"
 # Output: false
 # Explanation: The robot moves left twice. It ends up two "moves" to the left of the origin. We return false because it is not at the origin at the end of its moves.
