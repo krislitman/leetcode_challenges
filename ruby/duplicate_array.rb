00d205a63afafd4dbc82d0201dbe029e8e379096
Kris Litman <kris.d.litman@gmail.com>
2022-05-06 20:02:39 -0400

    Added edge case

--- ruby/duplicate_array.rb
+++ ruby/duplicate_array.rb
@@ -23,7 +23,7 @@
 
 
 def remove_duplicates(nums)
-    nums.uniq!.length
+    nums.length > 1 ? nums.uniq!.length : 1
 end
 
 # Example 1:
@@ -42,4 +42,4 @@ print remove_duplicates([0,0,1,1,1,2,2,3,3,4])
 # Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
 # It does not matter what you leave beyond the returned k (hence they are underscores).
 
-
+print remove_duplicates([1])
