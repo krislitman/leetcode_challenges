8bf1162fdbd8eda0aee44d7b15c186fd93ee4ac5
Kris Litman <kris.d.litman@gmail.com>
2022-05-16 22:22:46 -0400

    First test passing

--- ruby/ransom_note.rb
+++ ruby/ransom_note.rb
@@ -10,18 +10,28 @@ require "minitest/autorun"
 # return true if it can be, false otherwise
 
 def can_construct(ransom_note, magazine)
-
+    l = ransom_note.length
+    c = 0
+    ransom_dup = ransom_note.split("")
+    magazine.split("").each_with_index do |element, letter|
+        if ransom_dup.include?(letter)
+            c += 1
+            i = ransom_dup.index(letter)
+            ransom_dup.delete(i)
+        end
+    end
+    l == c ? true : false
 end
 
 
 class RansomNoteTest < Minitest::Test
     def test_case_one
-        assert_equal(can_construct("a", "b"), false)
+        assert_equal(false, can_construct("a", "b"))
     end
     # def test_case_two
-    #     assert_equal(can_construct("aa", "ab"), false)
+    #     assert_equal(false, can_construct("aa", "ab"))
     # end
     # def test_case_three
-    #     assert_equal(can_construct("aa", "aab"), true)
+    #     assert_equal(true, can_construct("aa", "aab"))
     # end
 end
